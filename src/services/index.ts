e9fec8d4e220a0034a8226fd11a2b53a36f14bff
stanah <foo.exe@gmail.com>
2025-09-06 05:08:38 +0900

    chore(exports): re-export built-in document types

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './parserService';
 // fileServiceは削除されました（VSCode専用になったため）
 export * from './schemaManager';
 export * from './documentTypes';
+export * from './documentTypes.builtins';
